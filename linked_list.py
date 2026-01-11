7a9960192fb2eb983be966649973ed96112a7aec
MedicalMoose <MedicalMoose@proton.me>
2026-01-11 09:27:11 +0300

    Jan 11

---
+++ linked_list.py
@@ -0,0 +1,37 @@
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' -> '
+            temp_node = temp_node.next
+        return result
+
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+
+
+new_linked_list = LinkedList()
+for i in range(10, 21):
+    new_linked_list.append(i)
+
+print(new_linked_list.head.value)
+print(new_linked_list.length)
+print(new_linked_list)
