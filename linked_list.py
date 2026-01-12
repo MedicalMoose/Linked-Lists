fe1faeb44417c3bb62165a8cf09c84217560da91
MedicalMoose <MedicalMoose@proton.me>
2026-01-12 09:49:22 +0300

    added prepend method

--- linked_list.py
+++ linked_list.py
@@ -27,10 +27,21 @@ class LinkedList:
         self.tail = new_node
         self.length += 1
 
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.tail is None:
+            self.tail = new_node
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+
 
 new_linked_list = LinkedList()
 for i in range(10, 21):
     new_linked_list.append(i)
+new_linked_list.append(21)
+new_linked_list.prepend(9)
 
 print(new_linked_list.head.value)
 print(new_linked_list.length)
